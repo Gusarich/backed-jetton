8a3488ed4c9f61570992112f1dc9b6d7a9a73804
Gusarich <danielsedovzzz@gmail.com>
2023-03-17 10:52:06 +0300

    copy files from token-contract repo and structure them

--- wrappers/BackedJetton.compile.ts
+++ wrappers/BackedJetton.compile.ts
@@ -1,5 +1,5 @@
 import { CompilerConfig } from '@ton-community/blueprint';
 
 export const compile: CompilerConfig = {
-    targets: ['contracts/backed_jetton.fc'],
+    targets: ['contracts/backed-jetton-minter.fc'],
 };
